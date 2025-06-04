f69a9b8d25ed30961e310de5b6e09123fef3a570
Arpit Pardesi <arpit.pardesi6@gmail.com>
2025-06-05 01:42:01 +0530

    minor changes

--- f1.py
+++ f1.py
@@ -155,11 +155,13 @@ class student(BaseModel):
    id: int
    name :str = Field(None, title="name of student", max_length=10)
    marks: List[int] = []
-   percent_marks: float
+   percent_marks: float\
+   
 class percent(BaseModel):
    id:int
    name :str = Field(None, title="name of student", max_length=10)
    percent_marks: float
+   
 @app.post("/marks", response_model=percent)
 async def get_percent(s1:student):
    s1.percent_marks=sum(s1.marks)/2
